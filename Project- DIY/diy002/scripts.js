8bb76d1cb0b0d4da2e4b9fae857c3d5544ce7388
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-08-24 15:35:46 -0300

    1101101

--- Project- DIY/diy002/scripts.js
+++ Project- DIY/diy002/scripts.js
@@ -1,5 +1,35 @@
 //variaveis
 
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const surName = document.querySelector("#surname");
+const birth = document.querySelector("#idate");
+const relationship = document.querySelector("#iec");
+const gender = document.querySelector("#sex");
+const state = document.querySelector("#state");
+const city = document.querySelector("#city");
+const msg = document.querySelector("#imsg");
+
+/*
+    console.log(
+        form,
+        nameInput, 
+        surName, 
+        birth, 
+        relationship, 
+        gender, 
+        state, 
+        city, 
+        msg
+    );
+*/
+
 //eventos
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault;
+
+    //ainda não sei como fazer pra usar os dados direito
+});
 
 //funções
