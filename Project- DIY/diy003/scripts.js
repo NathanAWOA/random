437097ea693926b4aa752c310d4a15184abece2b
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-08-29 15:42:30 -0300

    com erro

---
+++ Project- DIY/diy003/scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded" () => {
+    
+    const btnNot = document.querySelector("#btn-not");
+
+    btnNot.addEventListener("mouseover" () => {
+        
+        const x = Math.floor(Math.random() * (window.innerWidth - btnNot.clientWidth));
+        const y = Math.floor(Math.random() * (window.innerHeight - btnNot.clientHeight));
+
+
+        btnNot.style.position = "absolute";
+        btnNot.style.left = `${x}`;
+        btnNot.style.top = `${y}`;
+    });
+
+});
