8bb6b19784696fae78ca854011c767ab127aa35d
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-08-29 17:11:15 -0300

    test

--- Project- DIY/diy003/scripts.js
+++ Project- DIY/diy003/scripts.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+    const btnYep = document.querySelector("#yep");
     const btnNot = document.querySelector("#btn-not");
+    const btnCopy = document.querySelector("#copy");
+    const pixKey = ["e76d0f33-4849-4014-b2ff-2ca8dc97d1a6",
+    "Chama no pix filhão"];
 
     btnNot.addEventListener("mouseover", () => {
         
@@ -13,4 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
         btnNot.style.top = `${y}px`;
     });
 
+    btnYep.addEventListener("click", function (eventoDeClick) {
+        window.location.href = "qr.jpeg";
+    });
+
+    //eventoDeClick();
+    
+    //copy btn
+    btnCopy.addEventListener("click", (e) => {
+        e.preventDefault; 
+
+        //console.log(pixKey[0]);
+        navigator.clipboard.writeText(pixKey[0]);
+    });
 });
