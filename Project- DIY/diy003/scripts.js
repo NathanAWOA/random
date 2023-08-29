bc394923641116c84718019e584c1d4c19748176
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-08-29 15:54:54 -0300

    entre aspas resolvido

--- Project- DIY/diy003/scripts.js
+++ Project- DIY/diy003/scripts.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded" () => {
+document.addEventListener("DOMContentLoaded", () => {
     
     const btnNot = document.querySelector("#btn-not");
 
-    btnNot.addEventListener("mouseover" () => {
+    btnNot.addEventListener("mouseover", () => {
         
         const x = Math.floor(Math.random() * (window.innerWidth - btnNot.clientWidth));
         const y = Math.floor(Math.random() * (window.innerHeight - btnNot.clientHeight));
 
 
         btnNot.style.position = "absolute";
-        btnNot.style.left = `${x}`;
-        btnNot.style.top = `${y}`;
+        btnNot.style.left = `${x}px`;
+        btnNot.style.top = `${y}px`;
     });
 
 });
