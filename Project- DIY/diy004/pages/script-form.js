296e141e2c2d51a2b373657253b4ebba217b6310
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-09-11 15:07:49 -0300

    alguns testes

---
+++ Project- DIY/diy004/pages/script-form.js
@@ -0,0 +1,65 @@
+function firstStage() {
+    const prof = document.querySelector("#prof");
+    const profExtra = document.querySelector("#prof-extra");
+    const materias = document.querySelector("#materias");
+    const confirmBtn = document.querySelector("#confirm");
+    
+    /*
+    const manha = document.querySelector("#manha").value;
+    */
+    
+    const manha = document.querySelector("#manha");
+    const tarde = document.querySelector("#tarde");
+    const noite = document.querySelector("#noite");
+    
+    var periodo = false;
+
+    
+
+    confirmBtn.addEventListener("click", () => {
+        
+        if(manha.checked === true && tarde.checked === false && noite.checked === false) {
+            periodo = `${manha.value}`;
+
+            console.log(`Aula de ${materias.value} dos professores ${prof.value} e ${profExtra.value} no período da ${periodo}`);
+
+        } else if(manha.checked === true && tarde.checked === true && noite.checked === false) {
+            periodo = `${manha.value} e da ${tarde.value}`;
+
+            console.log(`Aula de ${materias.value} dos professores ${prof.value} e ${profExtra.value} no período da ${periodo}`);
+
+        }else if(manha.checked === true && tarde.checked === true && noite.checked === true) {
+            periodo = `${manha.value}, ${tarde.value} e ${noite.value}`;
+
+            console.log(`Aula de ${materias.value} dos professores ${prof.value} e ${profExtra.value} no período da ${periodo}`);
+
+        }else if(manha.checked === true && tarde.checked === false && noite.checked === true) {
+            periodo = `${manha.value} e da ${noite.value}`;
+
+            console.log(`Aula de ${materias.value} dos professores ${prof.value} e ${profExtra.value} no período da ${periodo}`);
+
+        }
+
+        if(manha.checked === false && tarde.checked === true && noite.checked === false) {
+            periodo = `${tarde.value}`;
+
+            console.log(`Aula de ${materias.value} dos professores ${prof.value} e ${profExtra.value} no período da ${periodo}`);
+
+        } else if(manha.checked === false && tarde.checked === true && noite.checked === true) {
+            periodo = `${tarde.value} e da ${noite.value}`;
+
+            console.log(`Aula de ${materias.value} dos professores ${prof.value} e ${profExtra.value} no período da ${periodo}`);
+
+        }
+
+        if(manha.checked === false && tarde.checked === false && noite.checked === true) {
+            periodo = `${noite.value}`;
+
+            console.log(`Aula de ${materias.value} dos professores ${prof.value} e ${profExtra.value} no período da ${periodo}`);
+
+        }
+
+});
+};
+
+firstStage();
