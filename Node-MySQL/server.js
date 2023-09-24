82a7a388db67406bf43154049930dc2f8bc0d37e
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-09-24 12:29:24 -0300

    tentando umas paradas

---
+++ Node-MySQL/server.js
@@ -0,0 +1,92 @@
+//EXPRESS
+const express = require('express')
+const app = express()
+
+//SOCKET.IO
+const socket = require('socket.io')
+
+//PORT
+const PORT = process.env.PORT || 8083
+
+//HTTP
+const http = require('http')
+const httpServer = http.createServer(app)
+const io = socket(httpServer, {
+    path: '/socket.io'
+})
+
+//SEQUILIZE
+const Sequilize = require('sequelize')
+const sequilize = new Sequilize('lang', 'root', 'password123A$', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+const userId = []
+
+//ARRAY DOS DADOS VINDOS DO FRONT
+let wordsInfo = []
+
+//CONNECTION
+io.on('connection', (client) => {
+    console.log(`O usuário com o id "${client.id}" acabou de logar.`)
+    userId.push(client)
+
+    client.on('disconnect', () => {
+        userId.splice(userId.indexOf(client), 1)
+    })
+
+    
+    client.on('sendData', data => {
+    wordsInfo.push(data)
+    console.log(wordsInfo)
+    
+    })
+    
+
+})
+
+//MANIPULANDO DB COM SEQUELIZE
+sequilize.authenticate().then(function() {
+    console.log('Connect')
+}).catch(function(erro) {
+    console.log(`Fail: ${erro}`)
+})
+
+const lang = sequilize.define(
+    'words', {
+        hiragana: {
+            type: Sequilize.STRING
+        },
+        katakana: {
+            type: Sequilize.STRING
+        },
+        kanji: {
+            type: Sequilize.STRING
+        },
+        br: {
+            type: Sequilize.STRING
+        }
+})
+
+
+//NECESSARIO RODAR ISSO ANTES DE ADD DADOS AO DB
+/*lang.sync({force: true})*/
+
+//INSERT INTO + VALUES
+/*  
+    lang.create({
+        hiragana: "だれ",
+        katakana: "",
+        kanji: "誰",
+        br: "Quem"
+    })  
+*/
+
+//PATH
+    app.use(express.static(__dirname + '/pages'))
+
+//PORTA
+httpServer.listen(PORT, () => {
+    console.log(`Server running at http://localhost:8083`)
+})
