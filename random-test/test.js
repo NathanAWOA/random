80a06af87ca247fc3f29b337a371ff51d55f7c02
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-10-27 17:26:07 -0300

    testando

---
+++ random-test/test.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")({sigint:true})
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+console.log("Write something")
+
+let word = prompt("")
+
+wordSplited = word.split([])
+
+for(i = 0; i < wordSplited.length; i++){
+    console.log(wordSplited[i])
+}
