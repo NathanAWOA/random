a32239846eb8e1b5f88e3ca1470dae5f54edaccc
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-10-28 12:55:12 -0300

    nem sei mais

--- random-test/test.js
+++ random-test/test.js
@@ -8,11 +8,87 @@ let word = prompt("")
 wordSplited = word.split([])
 
 for(i = 0; i < wordSplited.length; i++){
-    if(wordSplited[i] === 'a'){
-        console.log("yep")
-    } else {
-        console.log('nop')
-    }
+    codify()
+}
 
-    /*for usando o j agr para passar pelo alphabet dentro desse for somar j mais 5 (ou qualquer que seja a chave de decodificação) depois criar um if que defini que se o valor de j ficar superior ao length de alphabet então ele passara primeiro por uma subtração onde sera feito j menos alphabet.length assim criando uma impressão de loop*/
+function codify(){
+    let key = 5
+    if(wordSplited[i] === 'a'){
+        console.log(alphabet[4])
+    }
+    if(wordSplited[i] === 'b'){
+        console.log(alphabet[key])
+    }
+    if(wordSplited[i] === 'c'){
+        console.log(alphabet[1+key])
+    }
+    if(wordSplited[i] === 'd'){
+        console.log(alphabet[2+key])
+    }
+    if(wordSplited[i] === 'e'){
+        console.log(alphabet[3+key])
+    }
+    if(wordSplited[i] === 'f'){
+        console.log(alphabet[4+key])
+    }
+    if(wordSplited[i] === 'g'){
+        console.log(alphabet[5+key])
+    }
+    if(wordSplited[i] === 'h'){
+        console.log(alphabet[6+key])
+    }
+    if(wordSplited[i] === 'i'){
+        console.log(alphabet[7+key])
+    }
+    if(wordSplited[i] === 'j'){
+        console.log(alphabet[8+key])
+    }
+    if(wordSplited[i] === 'k'){
+        console.log(alphabet[9+key])
+    }
+    if(wordSplited[i] === 'l'){
+        console.log(alphabet[10+key])
+    }
+    if(wordSplited[i] === 'm'){
+        console.log(alphabet[11+key])
+    }
+    if(wordSplited[i] === 'n'){
+        console.log(alphabet[12+key])
+    }
+    if(wordSplited[i] === 'o'){
+        console.log(alphabet[13+key])
+    }
+    if(wordSplited[i] === 'p'){
+        console.log(alphabet[14+key])
+    }
+    if(wordSplited[i] === 'q'){
+        console.log(alphabet[15+key])
+    }
+    if(wordSplited[i] === 'r'){
+        console.log(alphabet[16+key])
+    }
+    if(wordSplited[i] === 's'){
+        console.log(alphabet[17+key])
+    }
+    if(wordSplited[i] === 't'){
+        console.log(alphabet[18+key])
+    }
+    if(wordSplited[i] === 'u'){
+        console.log(alphabet[19+key])
+    }
+    if(wordSplited[i] === 'v'){
+        console.log(alphabet[20+key])
+    }
+    if(wordSplited[i] === 'w'){
+        console.log(alphabet[key-5])
+    }
+    if(wordSplited[i] === 'x'){
+        console.log(alphabet[key-4])
+    }
+    if(wordSplited[i] === 'y'){
+        console.log(alphabet[key-3])
+    }
+    if(wordSplited[i] === 'z'){
+        console.log(alphabet[key-2])
+    }
 }
